8b6f49111eed8c910c2f0f3b52bb7ee03128646c
unknown <hanwei.chicago@gmail.com>
2018-10-01 20:42:44 -0500

    add attributes to WeatherForecast class

--- src/app/models/weather-forecast.ts
+++ src/app/models/weather-forecast.ts
@@ -1,3 +1,21 @@
+import { shiftInitState } from "@angular/core/src/view";
+
 export class WeatherForecast {
-    constructor() {}
+  datetime: string;
+  temp: number;
+  maxTemp: number;
+  minTemp: number;
+  weatherDescription: string;
+  windDirectionFull: string;
+  windSpeed: number;
+
+  constructor() {
+    this.datetime = '';
+    this.temp = 0;
+    this.maxTemp = 0;
+    this.minTemp = 0;
+    this.weatherDescription = '';
+    this.windDirectionFull = '';
+    this.windSpeed = 0;
+  }
 }
